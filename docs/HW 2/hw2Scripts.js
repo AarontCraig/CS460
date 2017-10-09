b95fa498321d7e2e6370962eb85764e49d89fd8b
aarontcraig <acraig15@wou.edu>
2017-10-08 19:22:51 -0800

    More JS functions. Got typedName complete. Working on goBlack.

--- docs/HW 2/hw2Scripts.js
+++ docs/HW 2/hw2Scripts.js
@@ -1,12 +1,22 @@
 function clickMeButton() {
-    var input = document.getElementsByClassName("startHidden");
+    var input = document.getElementsByClassName("leftBars");
     var i;
-    for(i = 0; i < input.length; ++i) {
-        input[i].style.display = "block";
-    }
+        for(i = 0; i < input.length; ++i) {
+            input[i].style.display = "block";
+        }
 }
 function typedName() {
-    var lName = document.getElementById("lName");
-    //if(lName.length)
-    console.log(lName.textContent);
+    var fName = document.getElementById("fName");
+    var lName = document.getElementById("lName");  
+    if(lName.value.length > 2 && fName.value.length > 2) {
+        var rButton = document.getElementById("resultsButton");
+        rButton.style.display = "block";
+    }
+    else
+        document.getElementById("resultsButton").style.display = "none";
+}
+function goBlack() {
+    var i;
+    for(i = 1; i > 0; i -= .0001)
+        $("body").css("opacity", i);
 }
