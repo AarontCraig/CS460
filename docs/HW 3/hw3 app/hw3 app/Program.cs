fc253fdd8afa8799e0f40b68977517745227c9c0
aarontcraig <acraig15@wou.edu>
2017-10-10 20:22:54 -0800

    Created StackADTC.cs

--- docs/HW 3/hw3 app/hw3 app/Program.cs
+++ docs/HW 3/hw3 app/hw3 app/Program.cs
@@ -10,7 +10,30 @@ namespace hw3_app
     {
         static void Main(string[] args)
         {
+            /*Console.WriteLine("Hello World");
 
+            for(int i = 0; i < args.Length; ++i)
+            {
+                Console.WriteLine("Args {0} : {1}", i, args[i]);
+            }
+            string[] myArgs = Environment.GetCommandLineArgs();
+            Console.WriteLine(string.Join(", ", myArgs));
+
+            SayHello(); */
+
+            bool canIVote = true;
+            Console.WriteLine("Biggest float : {0}, ", float.MaxValue);
+            Console.WriteLine("Smallest float : {0}, ", float.MinValue);
+
+            Console.ReadLine();
+        }
+
+        private static void SayHello()
+        {
+            string name = "";
+            Console.Write("What is your name : ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello {0}", name);
         }
     }
 }
---
+++ docs/HW 3/hw3 app/hw3 app/StackADT.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class StackADT
+{
+	public StackADT()
+	{
+
+	}
+}
