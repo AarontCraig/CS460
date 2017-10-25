9e182abb0dae017419019d5f70aade8095148423
aarontcraig <acraig15@wou.edu>
2017-10-24 20:24:40 -0800

    Got page_2 up and running with form collection

--- docs/HW4/HW4(test2)/HW4_Test2/Controllers/HW4Controller.cs
+++ docs/HW4/HW4(test2)/HW4_Test2/Controllers/HW4Controller.cs
@@ -9,70 +9,67 @@ namespace HW4_Test2.Controllers
 {
     public class HW4Controller : Controller
     {
-        public void calculateTemp(string type, string temp)
+        public double CalculateTemp(string type, string temp)
         {
-            
-        }
-        public ActionResult Page_1()
-        {
-            string type = Request.Form["type"];
-            string temp = Request.Form["temp"];
-
-            calculateTemp(type, temp);
             if (type != null)
             {
-                Debug.WriteLine(type);
                 //Pull the temp from the form and try to convert it to a double
-                double dTemp;
+                double dTemp = 0;
                 try
                 {
                     dTemp = Convert.ToDouble(temp);
                 }
                 catch (FormatException)
                 {
-                    ViewBag.newTemp = null;
-                    return View();
+                    return 0.12345;
                 }
                 catch (OverflowException)
                 {
-                    ViewBag.newTemp = null;
-                    return View();
+                    return 0.12345;
                 }
-
 
                 double newTemp;
                 if (type.StartsWith("c") || type.StartsWith("C"))
                 {
                     newTemp = (dTemp * 1.8) + 32;
+                    return newTemp;
                 }
                 else if (type.StartsWith("f") || type.StartsWith("F"))
                 {
                     newTemp = (dTemp - 32) * .5556;
+                    return newTemp;
                 }
                 else
                 {
-                    ViewBag.newTemp = null;
-                    return View();
+                    return 0.12345;
                 }
-
-                ViewBag.newTemp = newTemp;
-
-                return View();
             }
             else
-                return View();
+            {
+                return 0.12345;
+            }
+        }
+        public ActionResult Page_1()
+        {
+            string type = Request.Form["type"];
+            string temp = Request.Form["temp"];
+
+            ViewBag.newTemp = CalculateTemp(type, temp);
+
+            return View();
         }
 
         [HttpGet]
         public ActionResult Page_2()
         {
+            ViewBag.newTemp = 0.12345; //Sets it to the default, which is null (in this case)
             return View();
         }
 
         [HttpPost]
         public ActionResult Page_2(FormCollection form)
         {
-            Debug.WriteLine(form.Get("temp"));
+            ViewBag.newTemp = CalculateTemp(form.Get("type"), form.Get("temp"));
             return View();
         }
     }
