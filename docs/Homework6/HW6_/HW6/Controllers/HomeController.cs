b23a58723403c66fda27356614b76def933c6f4b
Aaron Craig <acraig15@wou.edu>
2017-11-08 23:40:28 -0800

    Set up new VS project. Imported DB. Created a new controller.

---
+++ docs/Homework6/HW6_/HW6/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HW6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AdventureWorksContext db = new AdventureWorksContext();
+
+        public ActionResult Index()
+        {
+            Console.WriteLine(db.Products.OrderBy(p => p.Name).Take(10).ToList());
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ docs/Homework6/SQL_RestoreDB.sql
@@ -0,0 +1,8 @@
+﻿RESTORE FILELISTONLY
+FROM DISK = 'D:\OneDrive\WOU 2017 - 2018\Fall Term\cs460\docs\Homework6\AdventureWorks2014.bak'
+GO
+
+RESTORE DATABASE AdventureWorks  
+FROM DISK = 'D:\OneDrive\WOU 2017 - 2018\Fall Term\cs460\docs\Homework6\AdventureWorks2014.bak'  
+WITH MOVE 'AdventureWorks2014_Data' TO 'D:\OneDrive\WOU 2017 - 2018\Fall Term\cs460\docs\Homework6\AdventureWorks2014.mdf',  
+MOVE 'AdventureWorks2014_Log' TO 'D:\OneDrive\WOU 2017 - 2018\Fall Term\cs460\docs\Homework6\AdventureWorks2014.ldf'
