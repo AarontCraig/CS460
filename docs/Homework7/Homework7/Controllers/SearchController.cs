ba48d8f2c7e3ae6e87d3395f325073cede74702a
Aaron Craig <acraig15@wou.edu>
2017-11-29 09:45:42 -0800

    HW7: Got enter to work on search.

---
+++ docs/Homework7/Homework7/Controllers/SearchController.cs
@@ -0,0 +1,46 @@
+﻿using Homework7.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework7.Controllers
+{
+    public class SearchController : Controller
+    {
+        public ActionResult Index(string q, string rating)
+        {
+            string apiKey = System.Web.Configuration.WebConfigurationManager.AppSettings["GiphyKey"];
+            WebRequest request = WebRequest.Create("http://api.giphy.com/v1/gifs/search?q=" + q + "&limit=20&rating=" + rating + "&api_key=" + apiKey); //Requests the data from the url
+            WebResponse response = request.GetResponse(); //Gets everything back from that website into one object
+            Stream dataStream = response.GetResponseStream(); //Gets the response I want back, the json object
+            StreamReader reader = new StreamReader(dataStream); //Decodes the stream
+            string responseFromServer = reader.ReadToEnd(); //Saves it into a string
+
+            //Convert from json to c#
+            JObject parsedString = JObject.Parse(responseFromServer);
+
+            IList<JToken> listedJsonObject = parsedString["data"].Children().Values("images").Values("fixed_width").ToList(); //Jumping into the {} of the json object, could do this several times to get data out of inner and upper layers
+
+            IList<Models.Gif> giphyResultsList = new List<Gif>();
+            foreach (JToken result in listedJsonObject)
+            {
+                Gif found = result.ToObject<Gif>();
+                giphyResultsList.Add(found);
+            }
+
+            //Close where necessary
+            reader.Close();
+            response.Close();
+
+            Debug.WriteLine("Inside Ajax");
+
+            return Json(giphyResultsList, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
--- docs/Homework7/Homework7/Scripts/updateGifs.js
+++ docs/Homework7/Homework7/Scripts/updateGifs.js
@@ -22,11 +22,41 @@ function newSearch() {
     type: "GET",
     url: 
 })*/
+function update() {
+    var query = document.getElementById("searchBar").value;
+    var rating = 0;
+    if (document.getElementById("y").checked)
+        rating = "y";
+    else if (document.getElementById("g").checked)
+        rating = "g";
+    else if (document.getElementById("pg").checked)
+        rating = "pg";
+    else if (document.getElementById("pg-13").checked)
+        rating = "pg-13";
+    else
+        rating = "r";
+    $.ajax({
+        url: "/Home/Index",
+        data: {
+            q: query,
+            rating: rating
+        },
+        dataType: "json",
+        type: "POST",
+        success: function (data) {
+            $('.gif').remove();
+            for (var i = 0; i < data.length; ++i) {
+                $("#gifs").append("<img src=\"" + data[i].url + "\" " + "class=\"gif\" />")
+                $('#gifs img').css({
+                    'width': 200,
+                    'height': 200
+                });
+            }
+        }
+    })
+}
 
-$('#searchButton').click(function () {
-    alert("Pressed");
-})
-
-$('.glyphicon').click(function () {
-    alert("Pressed me");
-})
+function enterCheck(e) {
+    if (e.keyCode === 13)
+        update()
+}
