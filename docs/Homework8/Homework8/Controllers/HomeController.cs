1e4d42cb8b40ac08e9b136b9c85d739c7db0508b
Aaron Craig <acraig15@wou.edu>
2017-11-30 22:14:12 -0800

    Databse stopped working. Deleted it.

--- docs/Homework8/Homework8/Controllers/HomeController.cs
+++ docs/Homework8/Homework8/Controllers/HomeController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Homework8.Models;
 
 namespace Homework8.Controllers
 {
     public class HomeController : Controller
     {
+        private Art a = new Art();
+
         public ActionResult Index()
         {
-
+            
 
             return View();
         }
